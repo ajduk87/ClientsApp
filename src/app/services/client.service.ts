a063a35dc8719ae00f2453c2d6a2fbfcfb47ff6c
mpm <mpm1628@gmail.com>
2019-05-02 23:43:29 +0200

    ok

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from 'angularfire2/database';
 import { Observable } from 'rxjs';
 import { Client } from '../models/Client';
 
 @Injectable()
 export class ClientService {
-  clients: FirebaseListObservable<any[]>;
-  client: FirebaseObjectObservable<any>;
+  clients: AngularFireList<any[]>;
+  client: AngularFireObject<any>;
 
   constructor(
     public af:AngularFireDatabase
   ) { 
-    this.clients = this.af.list('/clients') as FirebaseListObservable<Client[]>;
+    this.clients = this.af.list('/clients') as AngularFireList<Client[]>;
   }
 
   getClients(){
