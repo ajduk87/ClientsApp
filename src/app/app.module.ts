c7616b38bffd00bcb00aed017b87854c4f204cfd
mpm <mpm1628@gmail.com>
2019-05-03 11:08:44 +0200

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,7 @@ import { ClientService } from './services/client.service';
   {path:'register', component:RegisterComponent},
   {path:'login', component:LoginComponent},
   {path:'add-client', component:AddClientComponent},
+  {path:'client/:id', component:ClientDetailsComponent},
 ];
 
 export const firebaseConfig = {
